69ad2865bddf3b3c8bdf1c669317a12d3d5c9ed7
Kharajae Million <Milliokp@mail.uc.edu>
2020-10-19 17:41:13 -0400

    View added - HTML/CSS stucture changes

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
+    <div id="login-nav"></div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h3 class="Menu-Header">MENU</h3>
+      <hr>
+      <router-link to="/">HOME</router-link> <br><br>
+      <router-link to="/about">DIRECTIONS</router-link><br><br>
+      <router-link to="/Schedule">SCHEDULE</router-link><br><br>
+      <router-link to="/Settings">SETTINGS</router-link>
     </div>
     <router-view/>
+    <div id="foot"> </div>
   </div>
 </template>
 
@@ -14,7 +20,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 #nav {
@@ -22,11 +28,36 @@
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  font-weight: 500;
+  color: #ffffff;
+  font-size: 24px;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #646464;
+}
+
+#nav{
+background: rgb(81, 83, 83);
+float: left;
+width: 250px;
+height: 777px;
+}
+#login-nav{
+  background-color: rgb(212, 22, 22);
+  height: 80px; 
+}
+.Menu-header{
+  margin-bottom: 0;
+}
+hr{
+  color: white;
+  margin-top: 0;
+}
+#foot{
+  background: rgb(44, 44, 44) ;
+  height: 80px;
+  position: relative;
+  bottom: -353px;
 }
 </style>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -19,7 +20,18 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
-  }
+  },
+  {
+    path: '/',
+    name: 'Schedule',
+    component: Home
+  },
+  {
+    path: '/',
+    name: 'Settings',
+    component: Home
+  },
+
 ]
 
 const router = new VueRouter({
