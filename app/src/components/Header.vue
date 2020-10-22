2adef18b134ba4ba81a14243671cba09f0d5ba4e
Smit4mw <Smit4mw@mail.uc.edu>
2020-10-22 16:10:31 -0400

    Added temporary logo to header

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
 <div>
-  <b-navbar toggleable="" type="dark" variant="danger" fixed="top">
 
-  <a href="javascript:void(0)" id="menu-toggle" v-b-toggle="'sidebar-fifi-toggle'">
+  <b-navbar toggleable="" type="dark" variant="danger" fixed="top">
+    <b-navbar-brand href="index.html" class="hidden-xs"><img src="../assets/LogoWayfinder.png" alt="Logo" height="50px"></b-navbar-brand>
+    <a href="javascript:void(0)" id="menu-toggle" v-b-toggle="'sidebar-fifi-toggle'">
     <span class="navbar-toggler-icon" aria-hidden="true"></span></a>
-    <b-navbar-brand href="index.html" class="hidden-xs">Accessible Wayfinding: McMappen Edition</b-navbar-brand>
     
      <div class="ml-auto mr-3">
       <b-button-group>
         <a href="#" class="btn btn-danger" role="button">Register</a>
         <a href="#" class="btn btn-danger" role="button">Login</a>
       </b-button-group>
-      
+
     </div>
   </b-navbar>
 
