aa7d7db209c44985a939b4b03dcb7be5934bc211
Smit4mw <Smit4mw@mail.uc.edu>
2020-10-23 12:16:54 -0400

    Updated brand/center logos, increased font size

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,36 +1,55 @@
 <template>
-<div>
+  <div>
 
-  <b-navbar toggleable="" type="dark" variant="danger" fixed="top">
-    <b-navbar-brand href="index.html" class="hidden-xs"><img src="../assets/LogoWayfinder.png" alt="Logo" height="50px"></b-navbar-brand>
-    <a href="javascript:void(0)" id="menu-toggle" v-b-toggle="'sidebar-fifi-toggle'">
-    <span class="navbar-toggler-icon" aria-hidden="true"></span></a>
-    
-     <div class="ml-auto mr-3">
-      <b-button-group>
-        <a href="#" class="btn btn-danger" role="button">Register</a>
-        <a href="#" class="btn btn-danger" role="button">Login</a>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <b-navbar toggleable="" variant="danger" fixed="top">
+
+      <b-navbar-brand href="index.html"><i class="fab fa-accessible-icon fa-3x" style="color: #fff"></i></b-navbar-brand>
+      <b-button-group class="btn btn-danger">
+      <a href="javascript:void(0)" id="menu-toggle" v-b-toggle="'sidebar-fifi-toggle'">
+      <i class="fas fa-bars fa-1x" style="color: #ffffff;"></i></a>
       </b-button-group>
 
-    </div>
-  </b-navbar>
+      <div class="mx-auto d-block">
+        <img src="../assets/McMappenLogo.png" class="img-fluid" alt="Header Logo"> 
+      </div>
 
-</div>
+    
+        <b-button-group> 
+          <a href="#" class="btn btn-danger" role="button">Register</a>
+          <a href="#" class="btn btn-danger" role="button">Login</a>
+        </b-button-group>
+        
+    </b-navbar>
+
+  </div>
 </template>
 
 <style scoped>
 
-.navbar-brand {
-  font-size: 15px;
-  margin: 0px;
+.btn {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+}
+.navbar-menu {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: normal;
+  color: #fff;
 }
 
 .btn-danger:hover, .btn-danger:focus, .btn-danger:active, .btn-danger.active, .open>.dropdown-toggle.btn-danger {
-    color: #fff !important;
+    color: #fff;
     background-color: #00000010 !important;
     border-color: #00000000 !important;
     outline: none !important;
     box-shadow: none !important;
+}
+
+@media screen and (max-width:480px) {  
+  .img-fluid img {
+    display: none;
+  }
 }
 </style>
 
