a8193be1a6ace28cd35a87d05d9ff5322eca5057
Smit4mw <Smit4mw@mail.uc.edu>
2020-10-25 16:34:11 -0400

    Header adjustment Please enter the commit message for your changes. Lines starting

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -4,21 +4,22 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
     <b-navbar toggleable="" variant="danger" fixed="top">
-
+      
       <b-navbar-brand href="index.html"><i class="fab fa-accessible-icon fa-3x" style="color: #fff"></i></b-navbar-brand>
-      <b-button-group class="btn btn-danger">
-      <a href="javascript:void(0)" id="menu-toggle" v-b-toggle="'sidebar-fifi-toggle'">
-      <i class="fas fa-bars fa-1x" style="color: #ffffff;"></i></a>
-      </b-button-group>
+    
 
       <div class="mx-auto d-block">
         <img src="../assets/McMappenLogo.png" class="img-fluid" alt="Header Logo"> 
       </div>
 
-    
         <b-button-group> 
           <a href="#" class="btn btn-danger" role="button">Register</a>
           <a href="#" class="btn btn-danger" role="button">Login</a>
+        </b-button-group>
+
+        <b-button-group class="btn btn-danger">
+          <a href="javascript:void(0)" id="menu-toggle" v-b-toggle="'sidebar-fifi-toggle'">
+          <i class="fas fa-bars fa-1x" style="color: #ffffff;"></i></a>
         </b-button-group>
         
     </b-navbar>
