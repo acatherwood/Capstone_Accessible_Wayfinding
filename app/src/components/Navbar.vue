e70bf29da1d7dd79c7890d0c015a796fca122f58
Anne Catherwood <annecatherwood@gmail.com>
2020-11-03 20:10:10 -0500

    wip

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,8 +1,21 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
       <a href="javascript:void(0)" id="menu-toggle" v-b-toggle="'sidebar-fifi-toggle'"><span class="navbar-toggler-icon mb-1 mr-sm-1" aria-hidden="true"></span></a>
-    <b-navbar-brand href="index.html">brand
-    </b-navbar-brand>
+    <!-- <b-navbar-brand href="index.html">brand
+    </b-navbar-brand> -->
+    <div>
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <b-navbar toggleable="" variant="danger" fixed="top">
+
+      <b-navbar-brand href="index.html"><i class="fab fa-accessible-icon fa-3x" style="color: #fff"></i></b-navbar-brand>
+    <div class="mx-auto d-block">
+        <img src="../assets/McMappenLogo.png" class="img-fluid" alt="Header Logo"> 
+      </div>
+    </b-navbar>
+
+  </div>
     <div class="container">
       <router-link to="/" class="navbar-brand">Accessible Wayfinding</router-link>
       <button
@@ -62,3 +75,27 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.btn {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+}
+.navbar-menu {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: normal;
+  color: #fff;
+}
+.btn-danger:hover, .btn-danger:focus, .btn-danger:active, .btn-danger.active, .open>.dropdown-toggle.btn-danger {
+    color: #fff;
+    background-color: #00000010 !important;
+    border-color: #00000000 !important;
+    outline: none !important;
+    box-shadow: none !important;
+}
+@media screen and (max-width:480px) {  
+  .img-fluid img {
+    display: none;
+  }
+}
+</style>
