9a916b1d632e16a9a557a4b266161f762777957c
Smit4mw <Smit4mw@mail.uc.edu>
2020-11-08 08:48:09 -0500

    WIP - updated logo

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -6,7 +6,7 @@
 
     <div class="brand">
         <router-link to="/" class = "nav-link">
-        <img src="../assets/McMappenLogo.png" class="img-fluid" alt="Header Logo"> 
+        <img src="../assets/McMappingLogo.png" class="img-fluid" alt="Header Logo"> 
      </router-link>
       </div>
 
@@ -33,7 +33,9 @@
             <i class="fas fa-bars fa-1x" style="color: #ffffff;"></i>
           </button>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Directions</a>
+            <a class="dropdown-item" href="#">Directions
+              
+            </a>
             <a class="dropdown-item" href="About">About Us</a>
             <a class="dropdown-item" href="https://github.com/acatherwood/Capstone_Accessible_Wayfinding/issues" target="_blank">Report Issues</a>
           </div>
