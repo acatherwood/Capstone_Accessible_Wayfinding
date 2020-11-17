2bb50640efcad97a6b6ac1e4cc90395eb92dd577
Anne Catherwood <annecatherwood@gmail.com>
2020-11-16 21:14:33 -0500

    move direction modal

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -44,36 +44,20 @@
         </b-button-group>
     </div>
   </b-navbar>  
-
-                  <div class="modal" id="directions">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title"></h5>
-                          <button class="close" data-dismiss="modal">&times;</button>
-                        </div>
-                        <div class="modal-body">
-                          <div class="list-group">
-                            <div class="d-flex w-100 justify-content-between">
-                              <h6>Directions</h6>
-
-                            </div>
-                          </div>
-                          <div class="modal-footer">
-                            <button class="btn btn-secondary" data-dismiss="modal">Close</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+<Directions/>
 
 </body>
 </template>
 
 <script>
+import Directions from "../modals/directions.vue"
+
 import { mapGetters } from "vuex";
 import firebase from "firebase";
 export default {
+   components: {
+    Directions
+  },
   computed: {
     ...mapGetters({
 // map `this.user` to `this.$store.getters.user`
---
+++ app/src/modals/directions.vue
@@ -0,0 +1,31 @@
+
+ <template>
+    <div class="modal" id="directions">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+        <button class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+        <div class="list-group">
+            <div class="d-flex w-100 justify-content-between">
+            <h6>Directions</h6>
+
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    </div>
+
+</template>
+
+<script>
+        export default {
+            name: 'modal'
+        };
+    </script>
