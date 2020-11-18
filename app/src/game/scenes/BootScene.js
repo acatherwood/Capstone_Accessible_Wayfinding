4a95eb2db7ff8acb12841864cc6af2102fc175de
moburke <hall3mi@mail.uc.edu>
2020-11-17 23:07:13 -0500

    Added elevator and restroom icons to bootscene

--- app/src/game/scenes/BootScene.js
+++ app/src/game/scenes/BootScene.js
@@ -7,6 +7,8 @@ import { Scene } from 'phaser'
 import tileset from '@/game/assets/bw.png'
 import map from '@/game/assets/floor.json'
 import dude from '@/game/assets/phaserguy.png'
+import elevator from '@/game/assets/elevator2.png'
+import restroom from '@/game/assets/restroom2.png'
 
 
 export default class BootScene extends Scene {
@@ -18,7 +20,9 @@ export default class BootScene extends Scene {
     //this.scene = this; // Handy reference to the scene (alternative to `this` binding)
     this.load.image('tileset', tileset);
     this.load.tilemapTiledJSON('map', map);
-    this.load.image('phaserguy', dude); }
+    this.load.image('phaserguy', dude); 
+    this.load.image('elevator', elevator);
+    this.load.image('restroom', restroom);}
 
   create () {
     this.scene.start('PlayScene')
