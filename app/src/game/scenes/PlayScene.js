ebd0d165ccf5bfba5cd53ac9432baee01d877ddc
overmywm <55060771+overmywm@users.noreply.github.com>
2021-02-22 19:33:53 -0500

    Updated Playscene with line clearing
    Line clearing is overshadowed by walls of text used for testing, I will remove much of that in later iterations.

--- app/src/game/scenes/PlayScene.js
+++ app/src/game/scenes/PlayScene.js
@@ -2,10 +2,41 @@ import { Scene } from 'phaser'
 import EasyStar from 'easystarjs'
 
 var Game = {};
+var graphics; var line;
 var canWalk = -1;
 var TilePixelCount = 16;
 var scalerSize = 0.15;
-var bathroomCoords = [[225, 420], [1125, 435] , [1520,280]]
+var testCoords = [[120, 300, "101A"],
+[120, 300, "101A"],
+[160, 288, "101B"],
+[220, 300, "101C"],
+[240, 336, "101D"],
+[225, 330, "101"],
+[281, 288, "110A"],
+[281, 242, "110B "],
+[285, 182, "110C "],
+[288, 144, "110D "],
+[288, 84, "110E "],
+[341, 56, "110F "],
+[388, 65, "110G "],
+[432, 48, "110H "],
+[513, 100, "110J "],
+[512, 160, "110K "],
+[518, 222, "110M "],
+[494, 286, "110N "],
+[418, 242, "110P "],
+[418, 174, "110Q"],
+[514, 2630, "397A"],
+[1644, 2630, "397B "],
+[155, 2680, "398A "],
+[569, 2627, "398B "],
+[1594, 2624, "398C"],
+[2000, 2692, "398D "],
+[992, 2785, "399"]];
+var routeMarker1 = [[0,0]];
+var routeMarker2 = [[0,0]];
+
+
 export default class PlayScene extends Scene {
   constructor () {
     super({ key: 'PlayScene' })
@@ -18,13 +49,15 @@ export default class PlayScene extends Scene {
   
 
   create = function(){
+    
+
     var elevatorIcon = this.add.image(520,328, 'elevator').setScale(1.5);
     elevatorIcon.setDepth(1);
     
     var elevatorIconTwo = this.add.image(1512,328,'elevator').setScale(1.5);
     elevatorIconTwo.setDepth(1);
 
-    //load restroom icon one, two, three, four and five
+
     var restroom1 =this.add.image(224,420,'restroom').setScale(1);
     restroom1.setDepth(1);
 
@@ -36,17 +69,16 @@ export default class PlayScene extends Scene {
 
     restroom3.setDepth(1);
     var demosWindow = this.add.image(0, 0, 'btnWindow').setOrigin(0).setScale(.6);
-    var floor0icon = this.add.sprite(50, 280, 'moveBTN', 0).setOrigin(0).setInteractive().setScale(.2).setScrollFactor(0);
+    var floor0icon = this.add.sprite(50, 340, 'moveBTN', 0).setOrigin(0).setInteractive().setScale(.2).setScrollFactor(0);
     var floor3icon = this.add.sprite(15, 30, 'floor3icon', 0).setOrigin(0).setInteractive().setScale(0.05).setScrollFactor(0);
     var floor2icon = this.add.sprite(15, 90, 'floor2icon', 0).setOrigin(0).setInteractive().setScale(0.05).setScrollFactor(0);
-    var johnRouteIcon = this.add.sprite(40, 220, 'restroomBTN', 0).setOrigin(0).setInteractive().setScale(0.15).setScrollFactor(0);
+    var floorGicon = this.add.sprite(15, 210, 'floor2icon', 0).setOrigin(0).setInteractive().setScale(0.05).setScrollFactor(0);
+    var johnRouteIcon = this.add.sprite(40, 280, 'restroomBTN', 0).setOrigin(0).setInteractive().setScale(0.15).setScrollFactor(0);
     var floor1icon = this.add.sprite(15, 150, 'floor1icon', 0).setOrigin(0).setInteractive().setScale(0.05).setScrollFactor(0);
-    var demosContainer = this.add.container(0, 0, [ demosWindow, floor0icon, floor1icon, floor2icon, floor3icon, johnRouteIcon]);
+    var demosContainer = this.add.container(0, 0, [ demosWindow, floor0icon, floor1icon, floor2icon, floor3icon, floorGicon, johnRouteIcon]);
     demosContainer.setInteractive(new Phaser.Geom.Rectangle(0, 0, demosWindow.width, demosWindow.height), Phaser.Geom.Rectangle.Contains);
     
     this.input.setDraggable(demosContainer);
-
- 
 
     demosContainer.on('drag', function (pointer, dragX, dragY) {
         this.x = dragX;
@@ -71,20 +103,28 @@ export default class PlayScene extends Scene {
 
     });
 
+    floorGicon.on('pointerdown', function () {
+        var i = 0;
+        while(i < testCoords.length){
+        Game.scene.checkCollisionTester(testCoords[i]);
+        i++
+        }console.log(routeMarker1);
+    });
 
     floor0icon.on('pointerdown', function () {
        canWalk *= -1;
 
     });
-
+    line = new Phaser.Geom.Line( 0,0,678,678);
     johnRouteIcon.on('pointerdown', this.routeToRestroom );
     demosContainer.setDepth(1);
     demosContainer.setScrollFactor(0);
     
-    var getRouteBTN = this.add.sprite(screen.width/2, 90*8 - 327*scalerSize, 'restroomBTN', 0).setOrigin(0).setInteractive().setScale(scalerSize).setScrollFactor(0);
+    var getRouteBTN = this.add.sprite(screen.width/2, scalerSize, 'restroomBTN', 0).setOrigin(0).setInteractive().setScale(scalerSize).setScrollFactor(0);
     getRouteBTN.on('pointerdown', this.routeToRestroom );
     getRouteBTN.setDepth(1);
     getRouteBTN.setScrollFactor(0);
+
 
     // Handles the clicks on the map to make the character move
     this.input.on('pointerup',this.handleClick);
@@ -144,19 +184,47 @@ export default class PlayScene extends Scene {
         if(properties[i].cost) this.finder.setTileCost(i+1, properties[i].cost); // If there is a cost attached to the tile, let's register it
     }
     this.finder.setAcceptableTiles(acceptableTiles);
+    graphics = this.add.graphics({ lineStyle: { width: TilePixelCount, color: 0x0000FF } });//needs to be declared here to become visible on map, here specifically because it breaks earlier in the create function
+
 };
 
     update = function(){
         var worldPoint = this.input.activePointer.positionToCamera(Game.scene.cameras.main);
 
-        // Rounds down to nearest tile
+        // Rounds down to nearest tile and marks it (for move on click mode only)
         var pointerTileX = this.map.worldToTileX(worldPoint.x);
         var pointerTileY = this.map.worldToTileY(worldPoint.y);
         this.marker.x = this.map.tileToWorldX(pointerTileX);
         this.marker.y = this.map.tileToWorldY(pointerTileY);
-        this.marker.setVisible(true);
-    };
+        this.marker.setVisible(true); 
+        //adds route lines after clearing 
+        graphics.clear();
+        graphics.beginPath();
+        graphics.moveTo(routeMarker1[0][0], routeMarker1[0][1]);
+        for(var i = 0; i < routeMarker1.length; i++){
+            graphics.lineTo(routeMarker1[i][0], routeMarker1[i][1]);
+        }
+        graphics.moveTo(routeMarker2[0][0], routeMarker2[0][1]);
+        for(var i = 0; i < routeMarker2.length; i++){
+            graphics.lineTo(routeMarker2[i][0], routeMarker2[i][1]);
+        }
+        graphics.strokePath(); 
 
+        
+            
+    };
+    checkCollisionTester = function(item){
+        var tile = this.map.getTileAt(Math.floor(item[0]/16), Math.floor(item[1]/16));
+        var testerDude = this.add.image( item[0], item[1],'dude').setScale(0.5).setDepth(1);
+        this.add.text(item[0], item[1], item[2], { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color:'#A52A2A', fontSize:16 });
+
+        if(tile.properties.collide == true){
+            console.log(tile.properties.collide+" : "+ item[0]+ " / " +item[1] );
+            testerDude.setTint(0xff0000);
+        }
+
+
+    };
     checkCollision = function(x,y){
         var tile = this.map.getTileAt(x, y);
         return tile.properties.collide == true;
@@ -168,7 +236,7 @@ export default class PlayScene extends Scene {
     };
 
     handleClick = function(pointer){
-        //pointer.camera.setZoom(.8);
+        //pointer.camera.setZoom(.5);
         if(canWalk === 1){
             var x = pointer.camera.scrollX + pointer.x;
             var y = pointer.camera.scrollY + pointer.y;
@@ -177,7 +245,7 @@ export default class PlayScene extends Scene {
             var toY = Math.floor(y/TilePixelCount);
             var fromX = Math.floor(this.scene.player.x/TilePixelCount);
             var fromY = Math.floor(this.scene.player.y/TilePixelCount);
-            console.log('going from ('+fromX+','+fromY+') to ('+toX+','+toY+')');//debugging and for map design
+            console.log('going from ('+fromX+','+fromY+') to ('+toX*16+','+toY*16+')');//debugging and for map design
             Game.scene.finder.findPath(fromX, fromY, toX, toY, function( path ) {
                 if (path === null) {
                     console.warn("Path was not found.");
@@ -211,7 +279,7 @@ export default class PlayScene extends Scene {
                     console.warn("Path was not found.");
                 } else {
                     console.log(path);
-                    Game.scene.moveCharacter(path);
+                    Game.scene.moveCharacter(path, 1);
                     console.log(path.length);
                 }
             });
@@ -227,21 +295,22 @@ export default class PlayScene extends Scene {
                     console.warn("Path was not found.");
                 } else {
                     console.log(path);
-                    tweenNumbers = path;
+                    //tweenNumbers = path;
+                    Game.scene.moveCharacter(path, 0);
                 }
             })
             var fromX2 = Math.floor(nearestElevator[parseInt(toData[0])][0]);
             var fromY2 = Math.floor(nearestElevator[parseInt(toData[0])][1]);
             var toX2 = Math.floor(toData[1]/TilePixelCount);
             var toY2 = Math.floor(toData[2]/TilePixelCount);
-            console.log('going from ('+fromX2+','+fromY2+') to ('+toX2+','+toY2+')');//debugging and for map design
+            console.log('going from ('+fromX2+','+fromY2+') to ('+toX2*16+ ','+toY2*16+')');//debugging and for map design
             Game.scene.finder.findPath(fromX2, fromY2, toX2, toY2, function( path2 ) {
                 if (path2 === null) {
                     console.warn("Path was not found.");
                 } else {
-                    console.log(path2);
-                    tweenNumbers = tweenNumbers.concat(path2);
-                    Game.scene.moveCharacter(tweenNumbers);
+                    console.log(path2 , 0);
+                    //tweenNumbers = tweenNumbers.concat(path2);
+                    Game.scene.moveCharacter(path2, routeMarker2);
                 }
             });
             this.scene.finder.calculate();
@@ -249,28 +318,25 @@ export default class PlayScene extends Scene {
     };
 
 
-    moveCharacter = function(path){
+    moveCharacter = function(path, booleanFlag){
+        var tempRouteMarker = [ [(path[0 ].x*TilePixelCount),(path[0].y*TilePixelCount)] ]
         // Sets up a list of tweens, one for each tile to walk, that will be chained by the timeline
         var tweens = [];
-        ///Game.scene.graphics.clear();// not sure what to actually clear with
         for(var i = 0; i < path.length-1; i++){
             var ex = path[i+1].x;
             var ey = path[i+1].y;
-
-            
-            if(Math.abs(path[i+1].y - path[i].y) < 100 ){
-                var line = new Phaser.Geom.Line( (path[i].x*TilePixelCount),(path[i].y*TilePixelCount),  (path[i+1].x*TilePixelCount),(path[i+1].y*TilePixelCount));
-
-                Game.graphics = this.add.graphics({ lineStyle: { width: TilePixelCount, color: 0x0000FF } });//change line width and color here
-                Game.graphics.strokeLineShape(line);
-            }
-            
+            tempRouteMarker.push( [(path[i].x*TilePixelCount),(path[i].y*TilePixelCount)] );
             tweens.push({
                 targets: this.player,
                 x: {value: ex*this.map.tileWidth, duration: 50},
                 y: {value: ey*this.map.tileHeight, duration: 50}
             });
         }
+        if(booleanFlag === 1){
+            routeMarker1 = tempRouteMarker;
+
+        }
+        routeMarker2 = tempRouteMarker;
         Game.scene.tweens.timeline({
             tweens: tweens
         });
