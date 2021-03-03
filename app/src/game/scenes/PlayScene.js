9fea02ad34ee77ff08e69342f75f586fa3173e5e
moburke <hall3mi@mail.uc.edu>
2021-03-02 20:11:56 -0500

    replaced floor 1 with updated version

--- app/src/game/scenes/PlayScene.js
+++ app/src/game/scenes/PlayScene.js
@@ -43,8 +43,8 @@ export default class PlayScene extends Scene {
     
     //load floor maps for 0-3
 
-    var floor1PNG = this.add.image(1028,330,'floor1PNG').setScale(1);
-    floor1PNG.setDepth(1);
+    var floor1PNGv2 = this.add.image(1028,330,'floor1PNGv2').setScale(1);
+    floor1PNGv2.setDepth(1);
 
     var floor2PNG = this.add.image(1090,1550,'floor2PNG').setScale(1);
     floor2PNG.setDepth(1);
@@ -101,7 +101,7 @@ export default class PlayScene extends Scene {
     var floorGicon = this.add.sprite(15, 210, 'floor2icon', 0).setOrigin(0).setInteractive().setScale(0.05).setScrollFactor(0);
     var johnRouteIcon = this.add.sprite(40, 280, 'restroomBTN', 0).setOrigin(0).setInteractive().setScale(0.15).setScrollFactor(0);
     var floor1icon = this.add.sprite(15, 150, 'floor1icon', 0).setOrigin(0).setInteractive().setScale(0.05).setScrollFactor(0);
-    var floor1PNG = this.add.sprite(0,0,'floor1PNG',0).setOrigin(0).setInteractive().setScale(1).setScrollFactor(0);
+    var floor1PNGv2 = this.add.sprite(0,0,'floor1PNGv2',0).setOrigin(0).setInteractive().setScale(1).setScrollFactor(0);
     var floor2PNG = this.add.image(0,0,'floor2PNG').setOrigin(0).setInteractive().setScale(.9).setScrollFactor(0);
     var floor3PNG = this.add.image(0,0,'floor3PNG').setOrigin(0).setInteractive().setScale(.9).setScrollFactor(0);
     var floor0PNGv2 = this.add.image(0,0,'floor0PNGv2').setOrigin(0).setInteractive().setInteractive(1).setScrollFactor(0);
